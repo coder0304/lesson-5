ee6f28337089a35b5eb50eb334b057c67c57c497
coder0304 <nellikatgowri@gmail.com>
2024-12-29 16:33:10 +0530

    a3

---
+++ a4.py
@@ -0,0 +1,7 @@
+num= 49
+num_sqrt=(num)
+
+print(f'The number is{num} and it is stored in num_sqrt{num_sqrt}')
+num= int(input("enter the number :"))
+num_sqrt=num**0.5
+print('The square root of %0.2f is %0.3f'%(num,num_sqrt))
